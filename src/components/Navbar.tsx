9c299ab22bc69f220bd61e657ee7ac146e0ad120
kewldan <40865857+kewldan@users.noreply.github.com>
2023-11-22 20:40:33 +0300

    Navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,54 +1,87 @@
-import Link from "next/link";
-import NavbarDropdown from "./NavbarDropdown";
-import {FaGithub} from "react-icons/fa6";
-import React from 'react';
+'use client';
 
-const PATHS = [
-    {path: "/", name: "Home"},
-    {path: "/projects", name: "Projects"}
-];
+import React, {Fragment} from 'react'
+import {Disclosure} from '@headlessui/react'
+import {FaXmark} from "react-icons/fa6";
+import {HiMenu} from "react-icons/hi";
+import {usePathname} from "next/navigation";
 
-export default function Navbar() {
+const navigation = [
+    {name: 'Home', href: '/'},
+    {name: 'Projects', href: '/projects'},
+]
+
+function classNames(...classes: any[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+    const pathname = usePathname();
+
     return (
-        <header className="sticky top-0 z-20 flex justify-center py-3 bg-inherit w-full">
-            <div className="mx-4 flex w-full max-w-7xl items-center justify-center text-left">
-                <NavbarDropdown paths={PATHS}/>
-                <Link href="/">
-                    <div className="flex items-center gap-2">
-                        <h1 className="text-lg font-bold text-slate-300 transition-all duration-300 hover:text-slate-200 md:text-xl ml-2">
-                            <p className="inline font-light text-gray-400">
-                                dev://
-                            </p>
-                            <p className="inline font-bold">
-                                kewldan
-                            </p>
-
-                        </h1>
+        <Disclosure as="nav" className="bg-[#161b20] w-full">
+            {({open}) => (
+                <>
+                    <div className="mx-auto max-w-4xl px-2 sm:px-6 lg:px-8 w-full">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                <Disclosure.Button
+                                    className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="absolute -inset-0.5"/>
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <FaXmark size={20}/>
+                                    ) : (
+                                        <HiMenu size={20}/>
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex flex-shrink-0 items-center">
+                                    <span className='font-bold text-gray-400'>dev://</span>
+                                    <span className='font-semibold text-gray-200'>kewldan</span>
+                                </div>
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className={classNames(
+                                                    (pathname === item.href) ? 'bg-[#1a232b] text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    'rounded-md px-3 py-2 text-sm font-medium'
+                                                )}
+                                                aria-current={(pathname === item.href) ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </Link>
-                <nav className="flex w-full justify-end gap-2 text-right">
-                    {PATHS.map(({path, name}) =>
-                        <Link
-                            key={path}
-                            href={path}
-                            className={`hidden rounded px-2 py-1.5 transition-all duration-300 hover:text-slate-300 md:block ${path === '/' ? 'active' : ''}`}
-                        >
-                            {name}
-                        </Link>
-                    )}
-                    <a
-                        href="/github"
-                        target="_blank"
-                        className="rounded bg-primary px-3 py-1.5 text-sm font-medium text-slate-200 transition-all duration-300 hover:bg-blue-300 md:text-base flex flex-row"
-                    >
-                        <FaGithub size={26}/>
 
-                        <p className="ml-2 inline">
-                            Github
-                        </p>
-                    </a>
-                </nav>
-            </div>
-        </header>
-    );
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+                            {navigation.map((item) => (
+                                <Disclosure.Button
+                                    key={item.name}
+                                    as="a"
+                                    href={item.href}
+                                    className={classNames(
+                                        item.href === pathname ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                        'block rounded-md px-3 py-2 text-base font-medium'
+                                    )}
+                                    aria-current={item.href === pathname ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                </Disclosure.Button>
+                            ))}
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )}
+        </Disclosure>
+    )
 }
