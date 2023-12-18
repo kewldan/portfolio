a9bc05ccb276a921b0e3404faabfee12b83c6c6b
kewldan <kewldanil1@gmail.com>
2023-12-19 00:53:28 +0300

    DRM update

--- src/app/api/drm/route.ts
+++ src/app/api/drm/route.ts
@@ -1,6 +1,7 @@
 const allowed: Record<string, { name: string }> = {
     '03560274-043C-0535-2906-1C0700080009': {name: '@kewldan'},
-    'D37E5897-83CA-EB11-80DE-088FC3027302': {name: '@kalkof'}
+    'D37E5897-83CA-EB11-80DE-088FC3027302': {name: '@kalkof'},
+    '03C00218-044D-05D8-6D06-CE0700080009': {name: '@kalkof-computer'}
 }
 
 export async function POST(request: Request) {
